fb40b7cfcf643488c0782b784595c208a5f27a73
DanielSRibeiro <daniel57.ribeiro@gmail.com>
2022-10-03 16:36:40 -0300

    Adicionado o modulo de di para camada de network

---
+++ app/src/main/java/com/example/marvelapp/framework/di/NetworkModule.kt
@@ -0,0 +1,55 @@
+package com.example.marvelapp.framework.di
+
+import com.example.marvelapp.BuildConfig
+import com.example.marvelapp.BuildConfig.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            if(BuildConfig.DEBUG){
+                HttpLoggingInterceptor.Level.BODY
+            } else HttpLoggingInterceptor.Level.NONE
+        }
+    }
+
+    @Provides
+    fun provideOkHttpClient(
+        httpLoggingInterceptor: HttpLoggingInterceptor
+    ): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .readTimeout(15, TimeUnit.SECONDS)
+            .connectTimeout(15, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Provides
+    fun provideGsonConverterFactory(): GsonConverterFactory {
+        return GsonConverterFactory.create()
+    }
+
+    @Provides
+    fun provideRetrofit(
+        client: OkHttpClient,
+        converterFactory: GsonConverterFactory
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(converterFactory)
+            .build()
+    }
+}
