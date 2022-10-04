cb5dea955b83bb39f82372c254519fa73240840d
DanielSRibeiro <daniel57.ribeiro@gmail.com>
2022-10-04 09:37:20 -0300

    Melhora organizaçao do projeto

--- app/src/main/java/com/example/marvelapp/framework/di/NetworkModule.kt
+++ app/src/main/java/com/example/marvelapp/framework/di/NetworkModule.kt
@@ -1,6 +1,6 @@
 package com.example.marvelapp.framework.di
 
-import com.example.core.data.network.interceptor.AuthorizationInterceptor
+import com.example.marvelapp.framework.network.interceptor.AuthorizationInterceptor
 import com.example.marvelapp.BuildConfig
 import com.example.marvelapp.BuildConfig.BASE_URL
 import dagger.Module
